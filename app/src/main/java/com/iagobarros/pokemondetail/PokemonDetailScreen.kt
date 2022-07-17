d631bddff87b50fba09142a32471015ea99cedb2
Iago Barros <iagobarrosgit@gmail.com>
2022-07-17 16:55:58 -0300

    corrigido bug de herdar do ViewModel

---
+++ app/src/main/java/com/iagobarros/pokemondetail/PokemonDetailScreen.kt
@@ -0,0 +1,18 @@
+package com.iagobarros.pokemondetail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun PokemonDetailScreen(
+    dominantColor: Color,
+    pokemonName: String,
+    navController: NavController,
+    topPadding: Dp = 20.dp,
+
+) {
+
+}
---
+++ app/src/main/java/com/iagobarros/pokemondetail/PokemonDetailViewModel.kt
@@ -0,0 +1,19 @@
+package com.iagobarros.pokemondetail
+
+import androidx.lifecycle.ViewModel
+import com.iagobarros.pokedex.data.remote.responses.Pokemon
+import com.iagobarros.pokedex.repository.PokemonRepository
+import com.iagobarros.pokedex.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonDetailViewModel @Inject constructor(
+    private val repository: PokemonRepository
+) : ViewModel() {
+
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon> {
+        return repository.getPokemonInfo(pokemonName)
+    }
+
+}
